f9ea351ca7215ad560b73e4d7454b19f5a590d0b
Paul Armstrong <paul83637@gmail.com>
2025-11-09 03:50:56 +0000

    switch to nova premier

--- packages/http-api/src/utilities/Nova.ts
+++ packages/http-api/src/utilities/Nova.ts
@@ -5,7 +5,7 @@ const bedrockRuntimeClient = lazy(() => new BedrockRuntimeClient())
 
 export async function askNova(prompt: String): Promise<string> {
 	const res = await bedrockRuntimeClient.send(new InvokeModelCommand({
-		modelId: "us.amazon.nova-micro-v1:0",
+		modelId: "us.amazon.nova-premier-v1:0",
 		body: JSON.stringify({ "messages": [{ "role": "user", "content": [{ "text": prompt }] }] })
 	}))
 	const resJson = JSON.parse(Buffer.from(res.body).toString())
